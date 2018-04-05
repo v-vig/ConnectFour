179e9dc08b0fbdae5b0f62ffcf644d52023e6a44
Veronica Vigneswaran <viggy470@gmail.com>
2018-04-05 10:33:11 +1000

    config complete

--- ConnectFour.py
+++ ConnectFour.py
@@ -1,7 +1,24 @@
-config = input() # read game config from stdin
+# sample input
+# .ryyrry,.rryry.,..y.r..,..y....,.......,....... red A 4
+
+config = input()
 initial_state = config.split(" ")[0] # separate initial state
 initial_state = initial_state.split(",") # separate initial state into rows
 
 state = []
 for x in initial_state:
     state.append(list(x))
+
+is_red_next = False
+if (config.split(" ")[1] == "red"):
+    is_red_next = True
+
+alpha_beta_ON = False
+if (config.split(" ")[2] == "A"):
+    alpha_beta_ON = True
+
+max_depth = config.split(" ")[3]
+
+
+def score(state, player):
+    "This prints a passed string into this function"
