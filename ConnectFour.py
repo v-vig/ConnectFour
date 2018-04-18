012b4a52cca7fcd752d403dafabf090692248d86
Veronica Vigneswaran <viggy470@gmail.com>
2018-04-18 14:26:16 +1000

    Score method debugged

--- ConnectFour.py
+++ ConnectFour.py
@@ -4,75 +4,7 @@
 # assume player is in the form "r" or "y"
 # as the board is presented UPSIDE DOWN
 import sys
-
-def score(state, player):
-    single, double, triple, quadruple, total = (0, 0, 0, 0, 0)
-    for y in range(len(state)):
-        for x in range(len(state[y])):
-            if (state[y][x] == player):
-                single+=1
-                # check bottom
-                if (y-1 < 0 or state[y-1][x] != player):
-                    counter = 1
-                    # see how far connection goes UPWARDS
-                    while (True):
-                        if (y+counter <= board_height-1 and state[y+counter][x] == player):
-                            counter += 1
-                        else:
-                            if (counter == 2 ):
-                                double +=1
-                            if (counter == 3):
-                                triple +=1
-                            if (counter == 4):
-                                quadruple +=1
-                            break
-
-                    # check top right
-                    if (y-1 < 0 or x+1 < board_width-1 and state[y-1][x+1] != player):
-                        counter = 1
-                        while (True):
-                            if (x-counter >= 0 and y+counter <= board_height-1 and state[y+counter][x-counter] == player):
-                                counter += 1
-                            else:
-                                if (counter == 2 ):
-                                    double +=1
-                                if (counter == 3):
-                                    triple +=1
-                                if (counter == 4):
-                                    quadruple +=1
-                                break
-
-                    # check bottom left
-                    if (y-1 < 0 or x - 1 >= 0 and state[y-1][x-1]!= player):
-                        counter = 1
-                        while (True):
-                            if (x+counter <= board_width-1 and y+counter <= board_height-1 and state[y+counter][x+counter] == player):
-                                counter +=1
-                            else:
-                                if (counter == 2 ):
-                                    double +=1
-                                if (counter == 3):
-                                    triple +=1
-                                if (counter == 4):
-                                    quadruple +=1
-                                break
-
-                # check left
-                if (x - 1 < 0 or state[y][x-1] != player):
-                    counter = 1
-                    while (True):
-                        if (x+counter <= board_width-1 and state[y][x+counter] == player):
-                            counter +=1
-                        else:
-                            if (counter == 2 ):
-                                double +=1
-                            if (counter == 3):
-                                triple +=1
-                            if (counter == 4):
-                                quadruple +=1
-                            break
-    total = single + 10*double + 100*triple + 1000*quadruple
-    return total
+from score import score
 
 def evaluation(state):
     return score(state, "r") - score(state, "y")
@@ -94,16 +26,4 @@ if (sys.argv[3] == "A"):
     alpha_beta_ON = True
 max_depth = sys.argv[4]
 
-# generate a game state tree of depth max_depth
-
-# perform minimax algorithm on the tree
-# return winning child
-class Node:
-    def _init_(self, score, state, player, children = None):
-        self.score = score(state, player)
-        self.children = children or []
-        self.parent = None
-
-    def generate_children(self, state):
-        for x in range(board_width):
-            def child(score, state, player):
+print("Score = " + str(score(state,"y")))
